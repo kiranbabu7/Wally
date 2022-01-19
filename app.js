5f94eec078d227b96cdac3c677c88a2318e54d42
Kiran Babu <kiranbabu7675@gmail.com>
2022-01-19 15:47:44 +0530

    feat: load more button added

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const APi_key = "563492ad6f9170000100000157130e94bd754d4b81beca8d4421471f"; //pe
 const input = document.querySelector(".search-bar");
 const form = document.querySelector(".search-form");
 const gallery = document.querySelector(".gallery");
+const moreBtn = document.querySelector(".more-btn");
 let query; //user input
-
+let page = 1;
+let currentSearch;
 //Updates user query
 input.addEventListener("input", (e) => (query = e.target.value));
 
 //Search
 form.addEventListener("submit", function (e) {
   e.preventDefault();
+  currentSearch = query;
   Searchphotos(query);
 });
-
+//Load more images
+moreBtn.addEventListener("click", loadmore);
 //Fetches data from API
 async function fetchdata(url) {
   const response = await fetch(url, {
@@ -31,23 +35,39 @@ function generatephotos(data) {
   data.photos.forEach((photo) => {
     const imgContainer = document.createElement("div");
     imgContainer.classList.add("img-container");
-    imgContainer.innerHTML = `<p>${photo.photographer}<p/><img src='${photo.src.landscape}'></img>`;
+    imgContainer.innerHTML = `<p>${photo.photographer}<p/><img src='${photo.src.portrait}'></img>`;
     gallery.appendChild(imgContainer);
   });
 }
 
 //by default loads popular images in site
 async function curatedphotos() {
-  data = await fetchdata("https://api.pexels.com/v1/curated?per_page=2");
+  data = await fetchdata("https://api.pexels.com/v1/curated?per_page=16");
   generatephotos(data);
 }
 
 async function Searchphotos(query) {
-  data = await fetchdata(
-    `https://api.pexels.com/v1/search?query=${query}&per_page=3`
-  );
+  //clear input on search and removes previous images
   gallery.innerHTML = "";
+  input.value = "";
+  data = await fetchdata(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=16`
+  );
+
   generatephotos(data);
 }
 
+async function loadmore() {
+  page++;
+  if (currentSearch) {
+    data = await fetchdata(
+      `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=16&page=${page}`
+    );
+  } else {
+    data = await fetchdata(
+      `https://api.pexels.com/v1/curated?per_page=16&page=${page}`
+    );
+  }
+  generatephotos(data);
+}
 curatedphotos();
